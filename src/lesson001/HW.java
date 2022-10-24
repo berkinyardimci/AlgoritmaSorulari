b17b4cc90fdec63b2b8d4309b5b3e0f937d1992e
Berkin YARDIMCI (BilgeAdam) <berkin.yardimci@bilgeadam.com>
2022-10-24 17:05:32 +0300

    Odevler

---
+++ src/lesson001/HW.java
@@ -0,0 +1,44 @@
+package lesson001;
+
+public class HW {
+	//Odev 1
+	// Girilen kilo değerinni diğer gezegenlerde kaç kilo olduğunu bulan program (en az 5 gezegen)
+	
+		//1- kilo değerini girdiğimizde bütün gezegenlerdeki değeri hesaplayıp yazdıran
+		
+		// Ağırlık = kütle * yerçekimi
+	
+		//örnk input --> kilo girin 70
+		//output --> ayda 80kg, marsta 95kg, saturnde 88kg....
+		
+		
+		//2- bu sefer hem kilo hem gezegen girdisi alıyoruz girdiğimiz gezegen değerine göre ekrana yazdırıyo
+		
+		// 1- ay
+		// 2- Saturn
+		// 3- Mart
+		// 4- Venüs
+		
+		//input --> kg girin, (75)
+		//input --> gezegen adı girin (ay)
+		//output--> aydaki kilonuz 85
+		
+		
+		
+		
+		// Ödev 2--> BMI Hesaplama
+	
+		// Odev 3 -->		// 1- Toplama
+							// 2- Çıakrma
+							// 3- Çarpma
+							// 4- Bölme
+	
+	//hangi işlemi yapmak isterseniz (1)
+	
+	// iki tane sayı girin (5,10)
+	
+	// 2 tane sayı alcaz kullanıcıdan
+	
+	// 1 se iki sayıyı topla
+	// 2 ise 2 sayıyı çıkar ....
+}
