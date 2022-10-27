2bdb8f5dc996a8cc6d966c75ed47118bf6972afc
Berkin YARDIMCI (BilgeAdam) <berkin.yardimci@bilgeadam.com>
2022-10-28 01:41:11 +0300

    odev2

---
+++ src/odev/Odev2.java
@@ -0,0 +1,50 @@
+package odev;
+
+public class Odev2 {
+	public static void main(String[] args) {
+		//Soru1  (20 puan)
+				//kullanıcıdan bir kelime alın
+				// eğer kelime a ile başlıyorsa kelimenin son indexini ekrana bastırın
+				
+				// eğer kelime b ile başlıyorsa ilk indexi "@" ile değiştirin
+				
+				//eğer a veya b ile başlamıyorsa kelimenin birinci indexini bastırın
+				
+				//replace("ilk index", "@")
+				//charAt()
+				//kelime.startsWith("a")
+				//length()
+				//********************************************
+				
+		//Soru2 (40puan)
+				// Swtich case ile 
+				//input --> mehmet
+				//input--> hangisi işlemi yapmak istiyosunuz
+				
+				System.out.println("1- Kelimeleri Büyük Harf Yap");
+			    System.out.println("2- kelimenin harflerini Küçük yap");
+			    System.out.println("3- kelimenin harflerinin arasına ? koy");
+			    //mehmet m?h?m?e?t
+			    System.out.println("4- kelimenin başındaki ve sonundaki boşlukları silin");
+			    //     mehmet   
+			    System.out.println("5- kelimenin içindeki bütün a harflerini ? ile değiştirin");
+			    //replace replaceAll("a", ?")
+			    System.out.println("6- kelimenin ilk üç indexinin alıp sonuna ... koyun");
+			    // mehmet
+			    //meh...
+			    //substring()
+			    
+		//Soru3 (40 puan)
+			    
+			    //Tahmin oyunu 
+				//rastGele bir sayı oluşturcaz
+			    //rastgele sayı oluşturma--> int rastGeleSayi = (int) (Math.random() * 100);
+				// o sayıyı tahmin etmeye çalışcaz
+				//tahmin ettiğimiz sayı bulcağımız sayıdan fazla ise tahmini azalt yazdırın
+				// az ise tahmini artır yazalım
+				//sayıyı bulduğumuzda kaçıncı seferde bulduğumuzu yazdıralım
+			    //Her yanlış tahminde döngüden çıkmadan bize tekrar tahminimizi sorsun .
+			    
+			    //while ile çözüm
+	}
+}
