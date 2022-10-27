509217e305e154b99a49b8bee0716eaded9424c8
Berkin YARDIMCI (BilgeAdam) <berkin.yardimci@bilgeadam.com>
2022-10-27 16:39:16 +0300

    4.gunSonHali

---
+++ src/lesson004/Question40.java
@@ -0,0 +1,21 @@
+package lesson004;
+
+public class Question40 {
+
+	public static void main(String[] args) {
+		
+		//kullanıcıdan bir kelime alın
+		// eğer kelime a ile başlıyorsa kelimenin son indexini ekrana bastırın
+		
+		// eğer kelime b ile başlamıyorsa ilk indexi "@" ile değiştirin
+		
+		//eğer a veya b ile başlamıyorsa kelimenin birinci indexini bastırın
+		
+		//replace("ilk index", "@")
+		//charAt()
+		//kelime.startsWith("a")
+		//length()
+
+	}
+
+}
